e2b3ca1b6b31e4e3b35eaace82c9181aae10af8b
Druval CR <druval@scaler.com>
2021-01-04 11:42:57 +0530

    remove error: undefined method 'merge' in paperclip

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,7 +2,7 @@ class Interview < ApplicationRecord
     has_and_belongs_to_many :participants
 
     has_attached_file :resume, styles: { thumbnail: "60x60#" }
-    validates_attachment :resume, content_type: "application/pdf"
+    validates_attachment_content_type :resume, content_type: "application/pdf"
 
     validates :title, presence: true
     validates :start_time, presence: true
